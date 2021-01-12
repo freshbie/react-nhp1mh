07520ab1bebd5dccfb9f33047dcde3cc0d5db93b
freshbie <sanjaaykishore@gmail.com>
2021-01-12 16:37:32 +1300

    1 file modified

--- app/account-details.jsx
+++ app/account-details.jsx
@@ -165,6 +165,192 @@ export const AccountDetails = (
       id={"employer"}
       name={"employer"}
       label={"Who is your employer ?"}
+      hint={"Employeer Name"}
+      component={FormInput}
+      validator={driverValidator}
+    />
+    <Field
+      key={"employer_address"}
+      id={"employer_address"}
+      name={"employer_address"}
+      hint={"Employer Address"}
+      component={FormInput}
+      validator={addressValidator}
+    />
+    <Field
+      key={"employer_suburb"}
+      id={"employer_suburb"}
+      name={"employer_suburb"}
+      hint={"Employer Suburb"}
+      component={FormInput}
+      validator={addressValidator}
+    />
+    <Field
+      key={"emp_duration"}
+      id={"emp_duration"}
+      name={"emp_duration"}
+      label={"How long have you been employed there ?"}
+      component={FormInput}
+      validator={driverValidator}
+    />
+    <br />
+    <p>
+      <b>Joint Applicant (if applicable)</b>
+    </p>
+    <Field
+      key={"j_title"}
+      id={"j_title"}
+      name={"j_title"}
+      label={"Title"}
+      layout={"horizontal"}
+      component={FormRadioGroup}
+      data={title}
+      validator={requiredValidator}
+    />
+    <Field
+      key={"j_FirstName"}
+      id={"j_FirstName"}
+      name={"j_FirstName"}
+      label={"First Name"}
+      component={FormInput}
+      validator={userNameValidator}
+    />
+    <Field
+      key={"j_LastName"}
+      id={"j_LastName"}
+      name={"j_LastName"}
+      label={"Last Name"}
+      component={FormInput}
+      validator={userNameValidator}
+    />
+    <Field
+      key={"j_DOB"}
+      id={"j_DOB"}
+      name={"j_DOB"}
+      label={"Date of Birth"}
+      component={FormInput}
+      type={"date"}
+      validator={dobValidator}
+    />
+    <Field
+      key={"j_email"}
+      id={"j_email"}
+      name={"j_email"}
+      label={"Email"}
+      hint={"Hint: Enter your personal email address."}
+      type={"email"}
+      component={FormInput}
+      validator={emailValidator}
+    />
+    <Field
+      key={"j_Address"}
+      id={"j_Address"}
+      name={"j_Address"}
+      label={"What is your Home Address ?"}
+      hint={"Street"}
+      component={FormInput}
+      validator={addressValidator}
+    />
+    <Field
+      key={"j_suburb"}
+      id={"j_suburb"}
+      name={"j_suburb"}
+      hint={"Suburb"}
+      component={FormInput}
+      validator={addressValidator}
+    />
+    <Field
+      key={"j_postcode"}
+      id={"j_postcode"}
+      name={"j_postcode"}
+      hint={"Post Code"}
+      component={FormInput}
+      validator={addressValidator}
+    />
+    <Field
+      key={"j_resident"}
+      id={"j_resident"}
+      name={"j_resident"}
+      label={"Are you a NZ Resident ?"}
+      layout={"horizontal"}
+      component={FormRadioGroup}
+      data={resident}
+      validator={requiredValidator}
+    />
+    <Field
+      key={"j_driverlicence"}
+      id={"j_driverlicence"}
+      name={"j_driverlicence"}
+      label={"What is your Driver Licence Number ?"}
+      component={FormInput}
+      validator={driverValidator}
+    />
+    <Field
+      key={"j_lihold"}
+      id={"j_lihold"}
+      name={"j_lihold"}
+      label={"What type of Licence do you hold ?"}
+      layout={"horizontal"}
+      component={FormRadioGroup}
+      data={lihold}
+      validator={requiredValidator}
+    />
+    <Field
+      key={"j_children"}
+      id={"j_children"}
+      name={"j_children"}
+      label={"How many dependent children do you have ?"}
+      component={FormInput}
+      validator={childValidator}
+    />
+    <Field
+      key={"j_emp"}
+      id={"j_emp"}
+      name={"j_emp"}
+      label={"Are you employed ?"}
+      layout={"horizontal"}
+      component={FormRadioGroup}
+      data={emp}
+      validator={requiredValidator}
+    />
+    <Field
+      key={"j_occupation"}
+      id={"j_occupation"}
+      name={"j_occupation"}
+      label={"What is your occupation ?"}
+      component={FormInput}
+      validator={driverValidator}
+    />
+    <Field
+      key={"j_employer"}
+      id={"j_employer"}
+      name={"j_employer"}
+      label={"Who is your employer ?"}
+      hint={"Employeer Name"}
+      component={FormInput}
+      validator={driverValidator}
+    />
+    <Field
+      key={"j_employer_address"}
+      id={"j_employer_address"}
+      name={"j_employer_address"}
+      hint={"Employer Address"}
+      component={FormInput}
+      validator={addressValidator}
+    />
+    <Field
+      key={"j_employer_suburb"}
+      id={"j_employer_suburb"}
+      name={"j_employer_suburb"}
+      hint={"Employer Suburb"}
+      component={FormInput}
+      validator={addressValidator}
+    />
+    <Field
+      key={"j_emp_duration"}
+      id={"j_emp_duration"}
+      name={"j_emp_duration"}
+      label={"How long have you been employed there ?"}
       component={FormInput}
       validator={driverValidator}
     />
