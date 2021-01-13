4585cc52ef22ab65d494dbabb9a60dd893d65fba
freshbie <sanjaaykishore@gmail.com>
2021-01-13 13:12:22 +1300

    2 files modified

--- app/data.jsx
+++ app/data.jsx
@@ -30,58 +30,7 @@ export const emp = [
   { label: "No, unemployed", value: "No, unemployed" }
 ];
 
-export const countries = [
-  "Albania",
-  "Andorra",
-  "Armenia",
-  "Austria",
-  "Azerbaijan",
-  "Belarus",
-  "Belgium",
-  "Bosnia & Herzegovina",
-  "Bulgaria",
-  "Croatia",
-  "Cyprus",
-  "Czech Republic",
-  "Denmark",
-  "Estonia",
-  "Finland",
-  "France",
-  "Georgia",
-  "Germany",
-  "Greece",
-  "Hungary",
-  "Iceland",
-  "Ireland",
-  "Italy",
-  "Kosovo",
-  "Latvia",
-  "Liechtenstein",
-  "Lithuania",
-  "Luxembourg",
-  "Macedonia",
-  "Malta",
-  "Moldova",
-  "Monaco",
-  "Montenegro",
-  "Netherlands",
-  "Norway",
-  "Poland",
-  "Portugal",
-  "Romania",
-  "Russia",
-  "San Marino",
-  "Serbia",
-  "Slovakia",
-  "Slovenia",
-  "Spain",
-  "Sweden",
-  "Switzerland",
-  "Turkey",
-  "Ukraine",
-  "United Kingdom",
-  "Vatican City"
-];
+export const cycle = ["Weekly", "Fornightly", "Monthly"];
 
 export const sizes = [
   "X-Small",
@@ -92,13 +41,12 @@ export const sizes = [
   "2X-Large"
 ];
 
-export const sports = [
-  "Baseball",
-  "Basketball",
-  "Cricket",
-  "Field Hockey",
-  "Football",
-  "Table Tennis",
-  "Tennis",
-  "Volleyball"
+export const purpose = [
+  "Car Purchase",
+  "Travel",
+  "Debt Conslidations",
+  "Moving Costs",
+  "House Rennovation",
+  "Car Repair",
+  "Other"
 ];
--- app/loan-details.jsx
+++ app/loan-details.jsx
@@ -2,54 +2,117 @@ import * as React from "react";
 
 import { Field } from "@progress/kendo-react-form";
 
+import { FormInput, FormDropDownList } from "./form-components.jsx";
+
 import {
-  FormInput,
-  FormAutoComplete,
-  FormRadioGroup,
-  FormTextArea
-} from "./form-components.jsx";
+  nameValidator,
+  requiredValidator,
+  driverValidator
+} from "./validators.jsx";
 
-import { nameValidator, requiredValidator } from "./validators.jsx";
-
-import { countries, genders } from "./data.jsx";
+import { cycle, purpose } from "./data.jsx";
 
 export const LoanDetails = (
   <div>
+    <br />
+    <p>
+      <b>Loan Details</b>
+    </p>
     <Field
-      key={"fullName"}
-      id={"fullName"}
-      name={"fullName"}
-      label={"Full Name"}
+      key={"amount"}
+      id={"amount"}
+      name={"amount"}
+      label={"How much would you like to borrow ?"}
       component={FormInput}
-      validator={nameValidator}
+      validator={driverValidator}
     />
     <Field
-      key={"countryselected"}
-      id={"countryselected"}
-      name={"countryselected"}
-      label={"Country"}
-      hint={"Hint: Only Eroupean countries"}
-      component={FormAutoComplete}
-      data={countries}
+      key={"term"}
+      id={"term"}
+      name={"term"}
+      label={"How much can you regularly repay?"}
+      component={FormDropDownList}
+      data={cycle}
       validator={requiredValidator}
     />
     <Field
-      key={"gender"}
-      id={"gender"}
-      name={"gender"}
-      label={"Gender"}
-      layout={"horizontal"}
-      component={FormRadioGroup}
-      data={genders}
+      key={"repay"}
+      id={"repay"}
+      name={"repay"}
+      placeholder={"Please enter the amount"}
+      component={FormInput}
+      validator={driverValidator}
+    />
+    <Field
+      key={"purpose"}
+      id={"purpose"}
+      name={"purpose"}
+      label={"What is the purpose of this loan?"}
+      component={FormDropDownList}
+      data={purpose}
       validator={requiredValidator}
     />
     <Field
-      key={"about"}
-      id={"about"}
-      name={"about"}
-      label={"About"}
-      optional={true}
-      component={FormTextArea}
+      key={"other_purpose"}
+      id={"other_purpose"}
+      name={"other_purpose"}
+      placeholder={"If other, please state the reason"}
+      component={FormInput}
+      validator={driverValidator}
+    />
+    <br />
+    <p>
+      <b>Please fill in the table below</b>
+    </p>
+    <br />
+    <p>
+      <b>Income</b>
+    </p>
+    <Field
+      key={"income_cycle"}
+      id={"income_cycle"}
+      name={"income_cycle"}
+      label={"Net income after tax"}
+      component={FormDropDownList}
+      data={cycle}
+      validator={requiredValidator}
+    />
+    <Field
+      key={"income"}
+      id={"income"}
+      name={"income"}
+      placeholder={"Amount $"}
+      component={FormInput}
+      validator={driverValidator}
+    />
+    <Field
+      key={"j_income_cycle"}
+      id={"j_income_cycle"}
+      name={"j_income_cycle"}
+      label={"Joint applicant's income"}
+      component={FormDropDownList}
+      data={cycle}
+      validator={requiredValidator}
+    />
+    <Field
+      key={"j_income"}
+      id={"j_income"}
+      name={"j_income"}
+      placeholder={"Amount $"}
+      component={FormInput}
+      validator={driverValidator}
+    />
+    <br />
+    <p>
+      <b>Total Income(Monthly)</b>
+    </p>
+    <Field
+      key={"t_income"}
+      id={"t_income"}
+      name={"t_income"}
+      placeholder={"Amount $"}
+      component={FormInput}
+      validator={driverValidator}
     />
   </div>
 );
